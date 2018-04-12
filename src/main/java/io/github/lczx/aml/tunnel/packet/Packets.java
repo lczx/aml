3c52a75b04b15656c49327ad401ff1045892f7ca
Luca Zanussi <luca.z@outlook.com>
2018-04-12 19:09:17 +0200

    Add ability to create new (empty) packets

--- src/main/java/io/github/lczx/aml/tunnel/packet/Packets.java
+++ src/main/java/io/github/lczx/aml/tunnel/packet/Packets.java
@@ -17,7 +17,10 @@
 package io.github.lczx.aml.tunnel.packet;
 
 import io.github.lczx.aml.tunnel.packet.buffer.ByteBufferPool;
+import io.github.lczx.aml.tunnel.packet.editor.PayloadEditor;
 
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
 public final class Packets {
@@ -25,6 +28,14 @@ public final class Packets {
     public static final int LAYER_DATA_LINK = 2;
     public static final int LAYER_NETWORK = 3;
     public static final int LAYER_TRANSPORT = 4;
+
+    private static byte[] IPV4_SKEL = { // Bare IPv4 header template
+            /*ver: 4, IHL: 5*/ 0x45, /*ToS: 0*/ 0x00, /*total len: 20*/ 0x00, 0x14, /*ident: 0*/ 0x00, 0x00,
+            /*don't fragment*/ 0x40, 0x00, /*TTL: 128*/ (byte) 0x80, /*proto: 0xFF*/ (byte) 0xff,
+            /*checksum: 0*/ 0x00, 0x00, /*src. addr.*/ 0x00, 0x00, 0x00, 0x00, /*dst. addr.*/ 0x00, 0x00, 0x00, 0x00 };
+
+    private static byte[] UDP_SKEL = { // Bare UDP header template
+            /*src. port*/ 0x00, 0x00, /*dst. port*/ 0x00, 0x00, /*length: 8*/ 0x00, 0x08, /*checksum: 0*/ 0x00, 0x00 };
 
     private Packets() { }
 
@@ -34,6 +45,38 @@ public final class Packets {
 
     public static Packet wrapBuffer(final int topLayerType, final ByteBuffer buffer) {
         return new PacketImpl(topLayerType, buffer);
+    }
+
+    public static Packet newRawV4Packet(final int protoId, final Inet4Address srcAddr, final Inet4Address dstAddr) {
+        final Packet p = wrapBuffer(LAYER_NETWORK, (ByteBuffer) createBuffer().put(IPV4_SKEL).flip());
+        p.getLayer(IPv4Layer.class).editor()
+                .setProtocolId(protoId).setSourceAddress(srcAddr).setDestinationAddress(dstAddr).commit();
+        return p;
+    }
+
+    public static Packet newDatagramPacket(final InetSocketAddress srcSock, final InetSocketAddress dstSock) {
+        final Packet p = newRawV4Packet(IPv4Layer.PROTOCOL_UDP,
+                (Inet4Address) srcSock.getAddress(), (Inet4Address) dstSock.getAddress());
+
+        // We could generate an header via editor but we can't change the length field; we use a template instead
+        final PayloadEditor e = p.getLayer(IPv4Layer.class).payloadEditor();
+        e.buffer(true).put(UDP_SKEL);
+        e.flipAndCommit();
+        p.getLayer(UdpLayer.class).editor()
+                .setSourcePort(srcSock.getPort()).setDestinationPort(dstSock.getPort()).commit();
+        return p;
+    }
+
+    public static Packet newSegmentPacket(final InetSocketAddress srcSock, final InetSocketAddress dstSock,
+                                          final long seqNr, final long ackNr, final int flags, final int windowSize) {
+        return newSegmentPacket(srcSock, dstSock, seqNr, ackNr, flags, windowSize, null);
+    }
+
+    public static Packet newSegmentPacket(final InetSocketAddress srcSock, final InetSocketAddress dstSock,
+                                          final long seqNr, final long ackNr, final int flags, final int windowSize,
+                                          final byte[] tcpOptions) {
+        // TODO: Implement TCP layer editor first
+        throw new UnsupportedOperationException("Not implemented (yet)");
     }
 
 }
