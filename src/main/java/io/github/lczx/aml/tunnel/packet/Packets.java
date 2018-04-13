ef75a2e21f9c23e488101e87f748583eb7b49472
Luca Zanussi <luca.z@outlook.com>
2018-04-13 12:18:50 +0200

    Packet factory can now build empty TCP segments

--- src/main/java/io/github/lczx/aml/tunnel/packet/Packets.java
+++ src/main/java/io/github/lczx/aml/tunnel/packet/Packets.java
@@ -29,12 +29,17 @@ public final class Packets {
     public static final int LAYER_NETWORK = 3;
     public static final int LAYER_TRANSPORT = 4;
 
-    private static byte[] IPV4_SKEL = { // Bare IPv4 header template
+    private static final byte[] IPV4_SKEL = { // Bare IPv4 header template
             /*ver: 4, IHL: 5*/ 0x45, /*ToS: 0*/ 0x00, /*total len: 20*/ 0x00, 0x14, /*ident: 0*/ 0x00, 0x00,
             /*don't fragment*/ 0x40, 0x00, /*TTL: 128*/ (byte) 0x80, /*proto: 0xFF*/ (byte) 0xff,
             /*checksum: 0*/ 0x00, 0x00, /*src. addr.*/ 0x00, 0x00, 0x00, 0x00, /*dst. addr.*/ 0x00, 0x00, 0x00, 0x00 };
 
-    private static byte[] UDP_SKEL = { // Bare UDP header template
+    private static final byte[] TCP_SKEL = { // Bare TCP header template}
+            /*src. port*/ 0x00, 0x00, /*dst. port*/ 0x00, 0x00, /*seq. nr.*/ 0x00, 0x00, 0x00, 0x00,
+            /*ack. nr.*/ 0x00, 0x00, 0x00, 0x00, /*data offset: 5 (20), no flags*/ 0x50, 0x00,
+            /*wnd. size: 65535*/ (byte) 0xFF, (byte) 0xFF, /*checksum: 0*/ 0x00, 0x00, /*urg. ptr.*/ 0x00, 0x00 };
+
+    private static final byte[] UDP_SKEL = { // Bare UDP header template
             /*src. port*/ 0x00, 0x00, /*dst. port*/ 0x00, 0x00, /*length: 8*/ 0x00, 0x08, /*checksum: 0*/ 0x00, 0x00 };
 
     private Packets() { }
@@ -75,8 +80,17 @@ public final class Packets {
     public static Packet newSegmentPacket(final InetSocketAddress srcSock, final InetSocketAddress dstSock,
                                           final long seqNr, final long ackNr, final int flags, final int windowSize,
                                           final byte[] tcpOptions) {
-        // TODO: Implement TCP layer editor first
-        throw new UnsupportedOperationException("Not implemented (yet)");
+        final Packet p = newRawV4Packet(IPv4Layer.PROTOCOL_TCP,
+                (Inet4Address) srcSock.getAddress(), (Inet4Address) dstSock.getAddress());
+
+        final PayloadEditor e = p.getLayer(IPv4Layer.class).payloadEditor();
+        e.buffer(true).put(TCP_SKEL);
+        e.flipAndCommit();
+        p.getLayer(TcpLayer.class).editor()
+                .setSourcePort(srcSock.getPort()).setDestinationPort(dstSock.getPort())
+                .setSequenceNumber(seqNr).setAcknowledgementNumber(ackNr)
+                .setFlags(flags).setWindowSize(windowSize).setOptions(tcpOptions).commit();
+        return p;
     }
 
 }
